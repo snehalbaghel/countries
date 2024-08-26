a2c26ac6e6c74018d2a0b9172cd09f953ea4cf70
Snehal Baghel <snehalbaghel@gmail.com>
2024-08-26 10:58:22 +0530

    fix: escape apost

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="hidden bg-muted md:flex md:col-span-2 w-full h-full items-center justify-center">
       <span className="font-light text-2xl">
-        Select a country to view it's details...
+        Select a country to view it&apos;s details...
       </span>
     </main>
   );
